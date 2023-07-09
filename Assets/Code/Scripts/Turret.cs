2ab11f9c689d466d8beb599266568a261a029cb0
StefanBarna <stefanbarna10@gmail.com>
2023-07-09 13:13:21 -0400

    projectiles
    Implemented projectiles. Also I'm pretty sure the targetting inherently prioritizes the bullet closest to the end of the path but I could be wrong and will have to continue to keep an eye out for the behaviour. Worst comes to worst I just have to expand on the targetting script which shouldn't be a big deal.

--- Assets/Code/Scripts/Turret.cs
+++ Assets/Code/Scripts/Turret.cs
@@ -6,19 +6,22 @@ using UnityEditor;
 public class Turret : MonoBehaviour {
 
     [Header("References")]
-    [SerializeField] private Transform turretRotationPoint;
     [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform firingPoint;
 
     [Header("Attribute")]
     [SerializeField] private float targettingRange = 3f;
+    [SerializeField] private float pps = 2f;    // projectiles per second
 
     private Transform target;
-    private bool facingRight = false;
+    private float timeSinceFire = 0f;
 
+    private bool facingRight = false;
     private Vector3 spriteScale;
     private bool isFlipping = false;
-    private float flipCnt;              // amount through which the flip has occurred
-    private float flipFactor = 20f;    // rate at which the flip occurs
+    private float flipCnt = 1f;        // amount through which the flip has occurred
+    private float flipFactor = 30f;    // rate at which the flip occurs
 
     void Start() {
         spriteScale = transform.localScale;
@@ -32,14 +35,28 @@ public class Turret : MonoBehaviour {
 
         FaceTarget();
         
-        if (!CheckTargetInRange())
+        if (!CheckTargetInRange()) { 
             target = null;
+        } else {
+            timeSinceFire += Time.deltaTime;
+            if (timeSinceFire >= 1f / pps) {
+                Shoot();
+                timeSinceFire = 0f;
+            }
+        }
+    }
+
+    private void Shoot() {
+        GameObject projectileObj = Instantiate(projectilePrefab, firingPoint.position, Quaternion.identity);
+        Projectile projectileScript = projectileObj.GetComponent<Projectile>();
+        projectileScript.SetTarget(target);
     }
 
     private void FindTarget() {
         RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targettingRange, (Vector2)transform.position, 0f, enemyMask);
 
         if (hits.Length > 0) {
+            // TODO: prioritize furthest enemy
             target = hits[0].transform;
         }
     }
@@ -53,7 +70,6 @@ public class Turret : MonoBehaviour {
             if ((transform.position.x - target.position.x > 0 && facingRight) ||
                 (transform.position.x - target.position.x <= 0 && !facingRight)) {
                 isFlipping = true;
-                flipCnt = 1f;
             }
         }
 
@@ -66,17 +82,15 @@ public class Turret : MonoBehaviour {
 
             if (flipCnt <= -1f) {
                 isFlipping = false;
-                flipCnt = 0;
+                flipCnt = 1f;
                 facingRight = !facingRight;
                 spriteScale = transform.localScale;
             }
         }
-
-        // TODO: prioritize furthest enemy
     }
 
-    private void OnDrawGizmosDelected() {
+    private void OnDrawGizmosSelected() {
         Handles.color = Color.cyan;
-        Handles.DrawWireDisc(transform.position, transform.forward, targettingRange, enemyMask);
+        Handles.DrawWireDisc(transform.position, transform.forward, targettingRange);
     }
 }
